c5ab3460ea4cc215d61deb42fc4069e8f6dcbd37
Ryan Gilland <rgilland@hbs.edu>
2021-10-25 11:02:41 -0400

    Update fuzzygram.py

--- fuzzygram.py
+++ fuzzygram.py
@@ -4,7 +4,7 @@ Currently supports either unigram or moving-bigram matching
 """
 
 
-def ratio(string1, string2, position="loose"):
+def ratio(string1, string2, type="vector"):
     if string1 == "" and string2 == "":
         return 1.0
     elif (
@@ -14,7 +14,7 @@ def ratio(string1, string2, position="loose"):
     ):
         return 0.0
     else:
-        if position == "loose":
+        if type == "vector":
             string1 = [string1[i : i + 2] for i in range(0, len(string1) - 1)]
             string2 = [string2[i : i + 2] for i in range(0, len(string2) - 1)]
             unionset = set(set(string1) | set(string2))
@@ -28,7 +28,43 @@ def ratio(string1, string2, position="loose"):
             norm2 = sum([i**2 for i in sim2])**(1/2)
             score = dot / (norm1 * norm2)
             return score
-        elif position == "strict":
+        elif type == "loose":
+            if len(string1) == len(string2):
+                string1 = [string1[i : i + 2] for i in range(0, len(string1) - 1)]
+                string2 = [string2[i : i + 2] for i in range(0, len(string2) - 1)]
+                dot = []
+                for entry in string1:
+                    if entry in string2:
+                        dot.append(1)
+                        string2.remove(entry)
+                    else:
+                        dot.append(0)
+                score = sum(dot) / ((len(string1)) * (len(string2))) ** (
+                    1 / 2
+                )
+                return score
+            else:
+                if len(string1) < len(string2):
+                    sstring = string1
+                    lstring = string2
+                elif len(string1) > len(string2):
+                    sstring = string2
+                    lstring = string1
+                sstring = [sstring[i : i + 2] for i in range(0, len(sstring) - 1)]
+                lstring = [lstring[i : i + 2] for i in range(0, len(lstring) - 1)]
+                lstring_comp = lstring[:]
+                dot = []
+                for entry in sstring:
+                    if entry in lstring_comp:
+                        dot.append(1)
+                        lstring_comp.remove(entry)
+                    else:
+                        dot.append(0)
+                score = sum(dot) / ((len(sstring)) * (len(lstring))) ** (
+                    1 / 2
+                )
+                return score
+        elif type == "strict":
             if len(string1) == len(string2):
                 string1 = [string1[i : i + 2] for i in range(0, len(string1) - 1)]
                 string2 = [string2[i : i + 2] for i in range(0, len(string2) - 1)]
