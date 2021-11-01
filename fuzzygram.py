6190d47ec5a56f0135208da9b9039f6e6b2324f8
Ryan Gilland <rgilland@hbs.edu>
2021-11-01 17:02:24 -0400

    Update fuzzygram.py

--- fuzzygram.py
+++ fuzzygram.py
@@ -22,7 +22,7 @@ def pairwise(iterable):
     return zip(a, b)
 
 
-def ratio(string1, string2, match="vector"):
+def ratio(string1, string2, type="vector"):
     """
     Compare both string inputs in their entirety.
     "match" will eventually determine what method is used to compare:
@@ -38,7 +38,7 @@ def ratio(string1, string2, match="vector"):
     ):
         return 0.0
     else:
-        if match == "vector":
+        if type == "vector":
             string1 = Counter(pairwise(string1))
             string2 = Counter(pairwise(string2))
             dot = sum(string1[i] * string2[i] for i in string1.keys())
@@ -47,7 +47,7 @@ def ratio(string1, string2, match="vector"):
             return dot / ((norm1 * norm2) ** (0.5))
 
 
-def partial_ratio(string1, string2, match="vector"):
+def partial_ratio(string1, string2, type="vector", method="window"):
     """
     Compare both string inputs within a window determined by the smaller
     string. A window the size of the smaller string is rolled across the larger
@@ -64,26 +64,27 @@ def partial_ratio(string1, string2, match="vector"):
     ):
         return 0.0
     elif len(string1) == len(string2):
-        return ratio(string1, string2, match)
+        return ratio(string1, string2, type)
     else:
-        if match == "vector":
-            if len(string1) < len(string2):
-                sstring = string1
-                lstring = string2
-            else:
-                sstring = string2
-                lstring = string1
-            bigram = Counter(pairwise(sstring))
-            snorm = sum(i ** 2 for i in bigram.values())
-            window_size = len(sstring)
-            target = Counter(pairwise(lstring[:window_size]))
-            target[(None, lstring[0])] += 1
-            target[(lstring[window_size - 2], lstring[window_size - 1])] -= 1
-            results = {}
-            for index, (old, new) in enumerate(window(lstring, window_size)):
-                target[old] -= 1
-                target[new] += 1
-                dot = sum(count * target[key] for key, count in bigram.items())
-                norm = (sum(i ** 2 for i in target.values() if i > 0) * snorm) ** 0.5
-                results[index] = dot / norm
-            return max(results.values())
+        if type == "vector":
+            if method == "window":
+                if len(string1) < len(string2):
+                    sstring = string1
+                    lstring = string2
+                else:
+                    sstring = string2
+                    lstring = string1
+                bigram = Counter(pairwise(sstring))
+                snorm = sum(i ** 2 for i in bigram.values())
+                window_size = len(sstring)
+                target = Counter(pairwise(lstring[:window_size]))
+                target[(None, lstring[0])] += 1
+                target[(lstring[window_size - 2], lstring[window_size - 1])] -= 1
+                results = {}
+                for index, (old, new) in enumerate(window(lstring, window_size)):
+                    target[old] -= 1
+                    target[new] += 1
+                    dot = sum(count * target[key] for key, count in bigram.items())
+                    norm = (sum(i ** 2 for i in target.values() if i > 0) * snorm) ** 0.5
+                    results[index] = dot / norm
+                return max(results.values())
