3c84d06c6767c16f107958e2ad3b0a2c252b8144
Ryan Gilland <rgilland@hbs.edu>
2021-12-08 10:17:08 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -26,102 +26,105 @@ def stn_firm_name(target, unabbreviate=False, nolegal=False, parentheses=False):
         ">"
     ]
     
-    if target is str:
+    if type(target) is str:
         # Convert to uppercase
-        target = target.upper()
+        retarget = target.upper()
         
         # Remove characters in parentheses
         if parentheses == True:
             print("Removing substrings in parentheses...")
-            target = re.sub("[\(\[].*?[\)\]]", "", target)
+            retarget = re.sub("[\(\[].*?[\)\]]", "", retarget)
         
         # Keep A/C, C/O, D/B/A, and F/K/A together before replace
         print("Standardizing fictitious name forms...")
-        target = re.sub(" A\/C( |$)", " AC ", target)
-        target = re.sub(" C\/O( |$)", " CO ", target)
-        target = re.sub(" D\/B\/A/?( |$)", " DBA ", target)
-        target = re.sub(" D\/B\/$", " DBA", target)
-        target = re.sub(" F\/K\/A/?( |$)", " FKA ", target)
+        retarget = re.sub(" A\/C( |$)", " AC ", retarget)
+        retarget = re.sub(" C\/O( |$)", " CO ", retarget)
+        retarget = re.sub(" D\/B\/A/?( |$)", " DBA ", retarget)
+        retarget = re.sub(" D\/B\/$", " DBA", retarget)
+        retarget = re.sub(" F\/K\/A/?( |$)", " FKA ", retarget)
         
         # Remove text after DBA/FKA
         print("Removing text after fictitious name forms...")
-        target = re.sub(" (DBA|FKA)( |$).*", "", target)
+        retarget = re.sub(" (DBA|FKA)( |$).*", "", retarget)
         
         # Remove "the"
         print("Removing occurrences of \"The\"...")
-        target = re.sub("^THE ", "", target)
-        target = re.sub(" THE\s*$", "", target)
+        retarget = re.sub("^THE ", "", retarget)
+        retarget = re.sub(" THE\s*$", "", retarget)
         
         # Remove spaces between single letter words
         print("Removing spaces from beginning of firm names...")
-        search = re.search("^([A-Z] ([A-Z] )+[A-Z]) ", target)
+        search = re.search("^([A-Z] ([A-Z] )+[A-Z]) ", retarget)
         if search is not None:
             name = search[0].replace(" ", "")
-            target = name + re.sub("^[A-Z] ([A-Z] )+[A-Z] ", " ", target)
+            retarget = name + re.sub("^[A-Z] ([A-Z] )+[A-Z] ", " ", retarget)
         else:
-            search = re.search("^([A-Z] [A-Z]) ", target)
+            search = re.search("^([A-Z] [A-Z]) ", retarget)
             if search is not None:
                 name = search[0].replace(" ", "")
-                target = name + re.sub("^[A-Z] [A-Z] ", " ", target)
+                retarget = name + re.sub("^[A-Z] [A-Z] ", " ", retarget)
             else:
-                search = re.search("^([A-Z] & [A-Z]) ", target)
+                search = re.search("^([A-Z] & [A-Z]) ", retarget)
                 if search is not None:
                     name = search[0].replace(" ", "")
-                    target = name + re.sub("^[A-Z] & [A-Z] ", " ", target)
+                    retarget = name + re.sub("^[A-Z] & [A-Z] ", " ", retarget)
         
         # Clean organizational forms
         print("Standardizing organizational forms...")
-        target = re.sub(" COMPANY( |$)", " CO ", target)
-        target = re.sub(" CORPORATION( |$)", " CORP ", target)
-        target = re.sub(" FEDERAL CREDIT UNION$", " FCU", target)
-        target = re.sub(" CREDIT UNION$", " CU", target)
-        target = re.sub(" ( A)? FEDERAL SAVINGS BANK *$", " FSB", target)
-        target = re.sub(" F S B *$", " FSB", target)
-        target = re.sub(" INCORPORATED( |$)", " INC ", target)
-        target = re.sub(" LIMITED LIABILITY CO *$", " LLC", target)
-        target = re.sub(" (LIMITED|LTD) PARTNERSHIP *$", " LP", target)
-        target = re.sub(" LIMITED *$", " LTD", target)
-        target = re.sub(" P L L C *$", " PLLC", target)
-        target = re.sub(" L L C *$", " LLC", target)
-        target = re.sub(" L L L P *$", " LLLP", target)
-        target = re.sub(" L L P *$", " LLP", target)
-        target = re.sub(" L P *$", " LP", target)
-        target = re.sub(" M D( |$)", " MD ", target)
-        target = re.sub(" NATIONAL ASSOCIATION *$", " NA", target)
-        target = re.sub(" N A *$", " NA", target)
-        target = re.sub(" P A *$", " PA", target)
-        target = re.sub(" PROFESSIONAL ASSOCIATION *$", " PA", target)
-        target = re.sub(" P L C *$", " PLC", target)
-        target = re.sub("( A)? STATE SAVINGS BANK *$", " SSB", target)
-        target = re.sub(" S S B *$", " SSB", target)
-        target = re.sub("(^| )U S A( |$)", " USA ", target)
+        retarget = re.sub(" COMPANY( |$)", " CO ", retarget)
+        retarget = re.sub(" CORPORATION( |$)", " CORP ", retarget)
+        retarget = re.sub(" FEDERAL CREDIT UNION$", " FCU", retarget)
+        retarget = re.sub(" CREDIT UNION$", " CU", retarget)
+        retarget = re.sub(" ( A)? FEDERAL SAVINGS BANK *$", " FSB", retarget)
+        retarget = re.sub(" F S B *$", " FSB", retarget)
+        retarget = re.sub(" INCORPORATED( |$)", " INC ", retarget)
+        retarget = re.sub(" LIMITED LIABILITY CO *$", " LLC", retarget)
+        retarget = re.sub(" (LIMITED|LTD) PARTNERSHIP *$", " LP", retarget)
+        retarget = re.sub(" LIMITED *$", " LTD", retarget)
+        retarget = re.sub(" P L L C *$", " PLLC", retarget)
+        retarget = re.sub(" L L C *$", " LLC", retarget)
+        retarget = re.sub(" L L L P *$", " LLLP", retarget)
+        retarget = re.sub(" L L P *$", " LLP", retarget)
+        retarget = re.sub(" L P *$", " LP", retarget)
+        retarget = re.sub(" M D( |$)", " MD ", retarget)
+        retarget = re.sub(" NATIONAL ASSOCIATION *$", " NA", retarget)
+        retarget = re.sub(" N A *$", " NA", retarget)
+        retarget = re.sub(" P A *$", " PA", retarget)
+        retarget = re.sub(" PROFESSIONAL ASSOCIATION *$", " PA", retarget)
+        retarget = re.sub(" P L C *$", " PLC", retarget)
+        retarget = re.sub("( A)? STATE SAVINGS BANK *$", " SSB", retarget)
+        retarget = re.sub(" S S B *$", " SSB", retarget)
+        retarget = re.sub("(^| )U S A( |$)", " USA ", retarget)
         
         # Remove symbols
         print("Replacing symbols...")
         for symbol in symbols:
-            target = target.replace(symbol, "")
+            retarget = retarget.replace(symbol, "")
         
         # Replace +
         print("Replacing \"+\" with \"&\"...")
-        target = re.sub("^A \+ ", "A+ ", target)
-        if bool(re.match("^A\+ ", target)):
-            target = re.sub("+", " & ", target)
+        retarget = re.sub("^A \+ ", "A+ ", retarget)
+        if bool(re.match("^A\+ ", retarget)):
+            retarget = re.sub("+", " & ", retarget)
         
         if unabbreviate == True:
             print("Unabbreviating...")
-            target = re.sub(" ASSN( |$)", " ASSOCIATION ", target)
-            target = re.sub(" BCH( |$)", " BEACH ", target)
-            target = re.sub(" INTL( |$)", " INTERNATIONAL ", target)
-            target = re.sub(" (MGMT|MGT)( |$)", " MANAGEMENT ", target)
-            target = re.sub(" MKT( |$)", " MARKET ", target)
-            target = re.sub(" SR?VCS( |$)", " SERVICES ", target)
+            retarget = re.sub(" ASSN( |$)", " ASSOCIATION ", retarget)
+            retarget = re.sub(" BCH( |$)", " BEACH ", retarget)
+            retarget = re.sub(" INTL( |$)", " INTERNATIONAL ", retarget)
+            retarget = re.sub(" (MGMT|MGT)( |$)", " MANAGEMENT ", retarget)
+            retarget = re.sub(" MKT( |$)", " MARKET ", retarget)
+            retarget = re.sub(" SR?VCS( |$)", " SERVICES ", retarget)
         
         if nolegal == True:
             print("Removing legal forms...")
-            target = re.sub(" (CORP|CO|INC|LLC|LLLP|LLP|LTD)\s*$", "", target)
+            retarget = re.sub(" (CORP|CO|INC|LLC|LLLP|LLP|LTD)\s*$", "", retarget)
         
         print("Removing whitespace...")
-        target = target.strip()
-        target = " ".join(target.split())
-
-    return target
+        retarget = retarget.strip()
+        retarget = " ".join(retarget.split())
+        
+        return retarget
+    
+    else:
+        return target
